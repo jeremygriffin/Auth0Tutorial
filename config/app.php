b383f576e71c11eea5ebc6acda220d389466114f
jgriffin <whatsites@gmail.com>
2019-01-24 00:33:09 -0500

    Fix: Add the Auth0 user/contract includes
    I didn't add the library includes, so the classes couldn't be
    instantiated.

--- config/app.php
+++ config/app.php
@@ -175,6 +175,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Auth0\Login\LoginServiceProvider::class,
+
     ],
 
     /*
@@ -223,7 +224,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Auth0' => Auth0\Login\Facade\Auth0::class
+        'Auth0' => Auth0\Login\Facade\Auth0::class,
 
     ],
 
