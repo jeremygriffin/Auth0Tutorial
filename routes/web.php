47a5d5a6880366436ce4a1030a7c23aa354dcb1c
jgriffin <whatsites@gmail.com>
2019-01-24 00:00:45 -0500

    Feat: Add login/logout for Auth0.
    Add the controller to manage logging in and out.
    Add the routes for logging in and logging out.

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,14 @@ Route::get(
     '\Auth0\Login\Auth0Controller@callback'
     )->name('auth0-callback');
 
+Route::get(
+    '/login',
+    'Auth\Auth0IndexController@login'
+    )->name('login');
+
+Route::get(
+    '/logout',
+    'Auth\Auth0IndexController@logout'
+    )->name('logout')
+    ->middleware('auth');
+
